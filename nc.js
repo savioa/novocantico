7c8080fb572ab653c643c8d4b0428f0ff399a153
Sávio Almeida <impessoal@elleralmeida.com.br>
2015-04-13 16:21:44 -0300

    TEste de pesquisa local

--- nc.js
+++ nc.js
@@ -51,6 +51,40 @@ $(document)
       .transition('scale in', 1000)
     ;
 
+    $('.ui.search')
+      .search({
+        source: content
+      })
+    ;
+
+    var content = [
+      { title: 'Andorra' },
+      { title: 'United Arab Emirates' },
+      { title: 'Afghanistan' },
+      { title: 'Antigua' },
+      { title: 'Anguilla' },
+      { title: 'Albania' },
+      { title: 'Armenia' },
+      { title: 'Netherlands Antilles' },
+      { title: 'Angola' },
+      { title: 'Argentina' },
+      { title: 'American Samoa' },
+      { title: 'Austria' },
+      { title: 'Australia' },
+      { title: 'Aruba' },
+      { title: 'Aland Islands' },
+      { title: 'Azerbaijan' },
+      { title: 'Bosnia' },
+      { title: 'Barbados' },
+      { title: 'Bangladesh' },
+      { title: 'Belgium' },
+      { title: 'Burkina Faso' },
+      { title: 'Bulgaria' },
+      { title: 'Bahrain' },
+      { title: 'Burundi' }
+      // etc
+    ];
+
     setInterval(changeSides, 3000);
 
   })
