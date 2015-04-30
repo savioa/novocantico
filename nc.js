f371169ebba24658d5f127c40d48c6b5a16f86c9
Sávio Almeida <impessoal@elleralmeida.com.br>
2015-04-29 23:31:50 -0300

    Revisão do leiaute

--- nc.js
+++ nc.js
@@ -1,24 +1,27 @@
 $(document)
-  .ready(function() {
+.ready(function() {
 
-var content = [
-    {
-        title: '1',
-        description: 'Justo é o Senhor em seus santos caminhos'
-    },
-    {
-        title: '12',
-        description: 'O Senhor está no seu santo templo'
-    }
-];
+  $('.ui.dropdown')
+  .dropdown()
+  ;
 
-    $('.ui.search').search({
-      type: 'standard',
-      source : content,
-      searchFields   : [
-        'title'
-      ],
-      searchFullText: false
-    });
-  })
-;
+  var content = [
+  {
+    title: '1',
+    description: 'Justo é o Senhor em seus santos caminhos'
+  },
+  {
+    title: '12',
+    description: 'O Senhor está no seu santo templo'
+  }
+  ];
+
+  $('.ui.search').search({
+    type: 'standard',
+    source : content,
+    searchFields   : [
+    'title'
+    ],
+    searchFullText: true
+  });
+});
