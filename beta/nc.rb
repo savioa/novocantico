ce2bbe225d429782f7b569aa26a5df8e820675f4
Sávio Almeida <impessoal@elleralmeida.com.br>
2015-06-10 11:58:53 -0300

    adivinha?

--- beta/nc.rb
+++ beta/nc.rb
@@ -46,13 +46,13 @@ Dir.foreach raiz do |hino|
     titulo = XPath.first(xml, 'hino/titulo').text
     titulo_original = XPath.first(xml, 'hino/titulo_original').nil? ? titulo : XPath.first(xml, 'hino/titulo_original').text
 
-    # montar_hash_origem 'hino/origem_letra', xml, hash_origem_letra, numero, titulo
-    # montar_hash_origem 'hino/origem_musica', xml, hash_origem_musica, numero, titulo
-    # montar_hash_simples 'hino/texto/estrofe/verso', xml, hash_primeiro_verso, numero
-    # montar_hash_simples 'hino/texto/estrofe/coro/verso', xml, hash_coro, numero
-    # montar_hash_simples 'hino/titulo_original', xml, hash_titulo_original, numero
-    # montar_hash_simples 'hino/primeiro_verso_original', xml, hash_primeiro_verso_original, numero
-    # montar_hash_origem 'hino/metrica', xml, hash_metrica, numero, titulo_original.nil? ? titulo : titulo_original
+    montar_hash_origem 'hino/origem_letra', xml, hash_origem_letra, numero, titulo
+    montar_hash_origem 'hino/origem_musica', xml, hash_origem_musica, numero, titulo
+    montar_hash_simples 'hino/texto/estrofe/verso', xml, hash_primeiro_verso, numero
+    montar_hash_simples 'hino/texto/estrofe/coro/verso', xml, hash_coro, numero
+    montar_hash_simples 'hino/titulo_original', xml, hash_titulo_original, numero
+    montar_hash_simples 'hino/primeiro_verso_original', xml, hash_primeiro_verso_original, numero
+    montar_hash_origem 'hino/metrica', xml, hash_metrica, numero, titulo_original.nil? ? titulo : titulo_original
     XPath.each(xml, 'hino/referencia_biblica') { |e| hash_referencia_biblica.adicionar e.attributes['id'][0, 2], [e.text, numero, titulo, e.attributes['id']] }
   end
 end
@@ -119,22 +119,31 @@ def gerar_saida_simples(hash, id, titulo)
   File.write 'indice/%s/index.html' % id, modelo.gsub('navegacao-indice', navegacao).gsub('conteudo-indice', conteudo).gsub('titulo-indice', titulo).gsub('navegacao-movel-indice', navegacao_movel)
 end
 
-# conteudo = ''
-# hash_metrica.sort.each do |chave, valor|
-#   conteudo << ' ' * 36 + '<li><span id=\'%s\'>%s</span>' % [chave[0], chave[0].gsub('_', '.')] + "\n"
-#   conteudo <<  ' ' * 40 + '<ul class=\'list-unstyled\'>' + "\n"
+gerar_saida_origem hash_origem_letra, 'origem-letra', 'Origem da Letra'
+gerar_saida_origem hash_origem_musica, 'origem-musica', 'Origem da Música'
+gerar_saida_simples hash_primeiro_verso, 'primeiro-verso', 'Primeiro Verso'
+gerar_saida_simples hash_coro, 'coro', 'Coro'
+gerar_saida_simples hash_titulo_original, 'titulo-original', 'Título Original'
+gerar_saida_simples hash_primeiro_verso_original, 'primeiro-verso-original', 'Primeiro Verso Original'
 
-#   valor.each do |e|
-#     conteudo << ' ' * 44 + '<li><a href=\'../../hino/%s/%s.xml\'>%s</a>: %s</li>' % [e[0], e[0], e[1].gsub(/[,;:.!?]$/, ''), e[0]] + "\n"
-#   end
+# Métrica
+conteudo = ''
+hash_metrica.sort.each do |chave, valor|
+  conteudo << ' ' * 36 + '<li><span id=\'%s\'>%s</span>' % [chave[0], chave[0].gsub('_', '.')] + "\n"
+  conteudo <<  ' ' * 40 + '<ul class=\'list-unstyled\'>' + "\n"
 
-#   conteudo << ' ' * 40 + '</ul>' + "\n"
-#   conteudo << ' ' * 36 + '</li>' + "\n"
-# end
+  valor.each do |e|
+    conteudo << ' ' * 44 + '<li><a href=\'../../hino/%s/%s.xml\'>%s</a>: %s</li>' % [e[0], e[0], e[1].gsub(/[,;:.!?]$/, ''), e[0]] + "\n"
+  end
 
-# modelo = File.open('indice.modelo.html', 'r:UTF-8', &:read)
-# File.write 'indice/metrica/index.html', modelo.gsub('conteudo-indice', conteudo).gsub('titulo-indice', 'Métrica').gsub('navegacao-indice', '').gsub('navegacao-movel-indice', '')
+  conteudo << ' ' * 40 + '</ul>' + "\n"
+  conteudo << ' ' * 36 + '</li>' + "\n"
+end
 
+modelo = File.open('indice.modelo.html', 'r:UTF-8', &:read)
+File.write 'indice/metrica/index.html', modelo.gsub('conteudo-indice', conteudo).gsub('titulo-indice', 'Métrica').gsub('navegacao-indice', '').gsub('navegacao-movel-indice', '')
+
+# Referência bíblica
 conteudo = ''
 hash_referencia_biblica.sort.each do |chave, valor|
   conteudo << ' ' * 36 + '<li><span id=\'%s\'>%s</span>' % [chave, chave] + "\n"
@@ -150,10 +159,3 @@ end
 
 modelo = File.open('indice.modelo.html', 'r:UTF-8', &:read)
 File.write 'indice/referencia-biblica/index.html', modelo.gsub('conteudo-indice', conteudo).gsub('titulo-indice', 'Referência Bíblica').gsub('navegacao-indice', '').gsub('navegacao-movel-indice', '')
-
-# gerar_saida_origem hash_origem_letra, 'origem-letra', 'Origem da Letra'
-# gerar_saida_origem hash_origem_musica, 'origem-musica', 'Origem da Música'
-# gerar_saida_simples hash_primeiro_verso, 'primeiro-verso', 'Primeiro Verso'
-# gerar_saida_simples hash_coro, 'coro', 'Coro'
-# gerar_saida_simples hash_titulo_original, 'titulo-original', 'Título Original'
-# gerar_saida_simples hash_primeiro_verso_original, 'primeiro-verso-original', 'Primeiro Verso Original'
