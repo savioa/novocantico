6b339e410dc6aaaeacf26b70b0d0b75df09c94d0
Sávio Almeida <saviornelas+github@gmail.com>
2022-10-27 11:11:22 -0300

    Estruturação

---
+++ src/Atualizar/Hino.cs
@@ -0,0 +1,139 @@
+namespace NovoCantico;
+
+using System.Diagnostics;
+using System.Xml.Linq;
+
+[DebuggerDisplay("{Numero,nq} - {Titulo,nq}")]
+public class Hino
+{
+    private readonly char[] terminais = new char[] { '.', ',', ';', ':', '?', '!' };
+
+    public Hino(XDocument xdHino)
+    {
+        XNamespace xn = xdHino.Root!.GetDefaultNamespace();
+
+        XElement xeHino = xdHino.Element(xn + "hino")!;
+
+        Numero = xeHino.Attribute("num")!.Value;
+
+        Titulo = xeHino.Attribute("tit")!.Value;
+
+        Metrica = xeHino.Attribute("met")!.Value;
+
+        Secao = xeHino.Attribute("sec")!.Value;
+
+        Assunto = xeHino.Attribute("ass")!.Value;
+
+        TitulosOriginais = xeHino.Elements(xn + "tit_ori").Select(p => p.Value).ToList();
+
+        PrimeirosVersosOriginais = xeHino.Elements(xn + "pri_ver_ori").Select(p => p.Value).ToList();
+
+        OrigemLetra = xeHino.Elements(xn + "ori_let").Select(l => new Origem(l)).ToList();
+
+        OrigemMusica = xeHino.Elements(xn + "ori_mus").Select(m => new Origem(m)).ToList();
+
+        ReferenciasBiblicas = xeHino.Elements(xn + "ref_bib").Select(r => new ReferenciaBiblica(r.Attribute("liv")!.Value, r.Attribute("cap")!.Value, r.Attribute("ver")!.Value)).ToList();
+
+        Letra = xeHino.Element(xn + "tex")!.Elements(xn + "est").Select(e => new Estrofe(e, xn)).ToList();
+
+        if (!TitulosOriginais.Any())
+        {
+            TitulosOriginais.Add(Titulo);
+        }
+
+        if (!PrimeirosVersosOriginais.Any())
+        {
+            PrimeirosVersosOriginais.Add(Letra[0].Versos[0].Texto.TrimEnd(terminais));
+        }
+    }
+
+    public string Numero { get; set; }
+
+    public string Titulo { get; set; }
+
+    public string Metrica { get; set; }
+
+    public string Secao { get; set; }
+
+    public string Assunto { get; set; }
+
+    public IList<string> TitulosOriginais { get; set; }
+
+    public IList<string> PrimeirosVersosOriginais { get; set; }
+
+    public IList<Origem> OrigemLetra { get; set; }
+
+    public IList<Origem> OrigemMusica { get; set; }
+
+    public IList<ReferenciaBiblica> ReferenciasBiblicas { get; set; }
+
+    public IList<Estrofe> Letra { get; set; }
+
+    [DebuggerDisplay("{DebuggerDisplay,nq}")]
+    public class Origem
+    {
+        public Origem(XElement xeOrigem)
+        {
+            XAttribute? xaAno = xeOrigem.Attribute("ano");
+
+            Identificador = xeOrigem.Attribute("id")!.Value;
+            Ano = xaAno != null ? int.Parse(xaAno.Value) : null;
+        }
+
+        public string Identificador { get; set; }
+
+        public int? Ano { get; set; }
+
+        private string DebuggerDisplay => $"{Identificador}{(Ano != null ? $", {Ano}" : string.Empty)}";
+    }
+
+    [DebuggerDisplay("{DebuggerDisplay,nq}")]
+    public class Estrofe
+    {
+        public Estrofe(XElement xeEstrofe, XNamespace xn)
+        {
+            Versos = xeEstrofe.Elements(xn + "ver").Select(v => new Verso(v)).ToList();
+
+            Coros = xeEstrofe.Elements(xn + "cor").Select(c => new Estrofe(c, xn)).ToList();
+        }
+
+        public IList<Verso> Versos { get; set; }
+
+        public IList<Estrofe> Coros { get; set; }
+
+        private string DebuggerDisplay => $"{Versos[0].Texto} [...]";
+
+        [DebuggerDisplay("{DebuggerDisplay,nq}")]
+        public class Verso
+        {
+            public Verso(XElement xeVerso)
+            {
+                Texto = xeVerso.Value;
+                Detalhe = xeVerso.Attribute("voz")?.Value;
+            }
+
+            public string Texto { get; set; }
+
+            public string? Detalhe { get; set; }
+
+            private string DebuggerDisplay => $"{(Detalhe != null ? $"[{Detalhe}] " : string.Empty)}{Texto}";
+        }
+    }
+
+    [DebuggerDisplay("{Livro,nq} {Capitulo,nq}.{Versiculos,nq}")]
+    public class ReferenciaBiblica
+    {
+        public ReferenciaBiblica(string livro, string capitulo, string versiculos)
+        {
+            Livro = livro;
+            Capitulo = capitulo;
+            Versiculos = versiculos;
+        }
+
+        public string Livro { get; set; }
+
+        public string Capitulo { get; set; }
+
+        public string Versiculos { get; set; }
+    }
+}
---
+++ src/Atualizar/Program.cs
@@ -0,0 +1,153 @@
+﻿using System.Xml;
+using System.Xml.Linq;
+using System.Xml.Schema;
+using System.Xml.XPath;
+using System.Xml.Xsl;
+using NovoCantico;
+
+Directory.SetCurrentDirectory("..\\..\\xml");
+
+XmlSchemaSet schemas = new();
+foreach (string caminhoXsd in Directory.EnumerateFiles("xsd"))
+{
+    schemas.Add("http://www.novocantico.com.br/schema/hino", caminhoXsd);
+}
+
+XmlNamespaceManager nsm = new(new NameTable());
+nsm.AddNamespace("xs", "http://www.w3.org/2001/XMLSchema");
+
+Dictionary<string, string> metricas = new();
+
+XDocument xdMetrica = XDocument.Load("xsd\\metrica.xsd");
+foreach (XElement itemMetrica in xdMetrica.XPathSelectElements("//xs:enumeration", nsm))
+{
+    string idMetrica = itemMetrica.Attribute("value")!.Value;
+
+    if (metricas.ContainsKey(idMetrica))
+    {
+        throw new Exception($"A métrica foi definida duas vezes: {idMetrica}");
+    }
+
+    string descricaoMetrica = itemMetrica.XPathSelectElement(".//xs:documentation", nsm)!.Value;
+
+    if (idMetrica != descricaoMetrica.Replace('.', '_').Replace('[', '_').Replace(']', '_'))
+    {
+        throw new Exception($"O identificador e a descrição da métrica não são compatíveis: {idMetrica} {descricaoMetrica}");
+    }
+
+    metricas.Add(idMetrica, descricaoMetrica);
+}
+
+Dictionary<string, (string Descricao, string Referencia)> origens = new();
+
+XDocument xdOrigem = XDocument.Load("xsd\\origem.xsd");
+foreach (XElement itemOrigem in xdOrigem.XPathSelectElements("//xs:enumeration", nsm))
+{
+    string idOrigem = itemOrigem.Attribute("value")!.Value;
+
+    if (origens.ContainsKey(idOrigem))
+    {
+        throw new Exception($"A origem foi definida duas vezes: {idOrigem}");
+    }
+
+    string descricaoOrigem = itemOrigem.XPathSelectElement(".//xs:documentation", nsm)!.Value;
+    string referenciaOrigem = itemOrigem.XPathSelectElement(".//xs:appinfo", nsm)!.Value;
+
+    if (referenciaOrigem.Contains(','))
+    {
+        string[] partes = referenciaOrigem.Split(", ");
+
+        if ($"{partes[1]} {partes[0]}" != descricaoOrigem)
+        {
+            throw new Exception($"A descrição e a referência da origem não são compatíveis: {descricaoOrigem} {referenciaOrigem}");
+        }
+    }
+
+    origens.Add(idOrigem, (descricaoOrigem, referenciaOrigem));
+}
+
+Dictionary<string, string> referencias = new();
+
+XDocument xdReferencia = XDocument.Load("xsd\\referencia_biblica.xsd");
+foreach (XElement itemReferencia in xdReferencia.XPathSelectElements("//xs:enumeration", nsm))
+{
+    string idReferencia = itemReferencia.Attribute("value")!.Value;
+
+    if (referencias.ContainsKey(idReferencia))
+    {
+        throw new Exception($"A referência foi definida duas vezes: {idReferencia}");
+    }
+
+    string descricaoReferencia = itemReferencia.XPathSelectElement(".//xs:documentation", nsm)!.Value;
+
+    referencias.Add(idReferencia, descricaoReferencia);
+}
+
+List<Hino> hinario = new();
+
+foreach (string caminhoXml in Directory.EnumerateFiles(Directory.GetCurrentDirectory(), "*.xml"))
+{
+    XDocument xdHino = XDocument.Load(caminhoXml);
+
+    string erro = string.Empty;
+    xdHino.Validate(schemas, (o, e) =>
+    {
+        erro = e.Message;
+    });
+
+    if (!string.IsNullOrEmpty(erro))
+    {
+        Console.WriteLine($"{Path.GetFileName(caminhoXml)} - {erro}");
+        return;
+    }
+
+    Hino hino = new(xdHino);
+    hinario.Add(hino);
+
+    XDocument html = new();
+
+    using (XmlReader xrXsl = XmlReader.Create("hino.xsl"))
+    {
+        XslCompiledTransform transformer = new();
+        transformer.Load(xrXsl);
+
+        string caminhoSaida = $"..\\pub\\{hino.Numero}";
+
+        if (!Directory.Exists(caminhoSaida))
+        {
+            Directory.CreateDirectory(caminhoSaida);
+        }
+
+        StringWriterUtf8 twSaida = new();
+
+        transformer.Transform(caminhoXml, null, twSaida);
+
+        string saida = twSaida.ToString();
+
+        saida = saida.Replace(" xmlns:nc=\"http://www.novocantico.com.br/schema/hino\"", string.Empty);
+        saida = saida.Replace($">{hino.Metrica}<", $">{metricas[hino.Metrica]}<");
+
+        foreach (Hino.Origem origem in hino.OrigemLetra.Union(hino.OrigemMusica))
+        {
+            saida = saida.Replace($">{origem.Identificador}<", $">{origens[origem.Identificador].Descricao}<");
+        }
+
+        foreach (Hino.ReferenciaBiblica referencia in hino.ReferenciasBiblicas)
+        {
+            saida = saida.Replace($">{referencia.Livro}#", $">{referencias[referencia.Livro]} ");
+        }
+
+        File.WriteAllText(caminhoSaida + "\\index.html", saida);
+    }
+}
+
+public class StringWriterUtf8 : StringWriter
+{
+    public override System.Text.Encoding Encoding
+    {
+        get
+        {
+            return System.Text.Encoding.UTF8;
+        }
+    }
+}
