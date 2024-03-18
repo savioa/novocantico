93aabce31ef3eb2bf74f9291ffc76f3058f4dbc7
Savio Ornelas Almeida <savio.almeida@serpro.gov.br>
2024-03-18 08:44:49 -0300

    Analytics

--- backup/js/cifra-2009-08-28.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-	$(".link_cifra").click(function() {
-  	$("#texto").toggle();
-		$("#cifra").toggle();
-		$("#mostrar").toggle();
-		$("#ocultar").toggle();
-	});
-});
